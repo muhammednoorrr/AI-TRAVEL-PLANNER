66822f6e389cf4cb690be31eba93601b2773a94a
Mubarek Adem <mubarekademy@gmail.com>
2025-01-12 21:45:26 +0300

    initiated landing page , create-trip page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import CreateTrip from "./create-trip";
+import Header from "./components/custom/Header";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create-trip",
+    element: <CreateTrip />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Header />
+    <RouterProvider router={router} />
+  </StrictMode>
+);
